e21d9e5c0d870804ef2d9ca1bab50d0e18763afa
Mayra Moreno <malejandramp13@gmail.com>
2022-09-08 18:03:13 -0500

    falta cositas

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,8 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
   end
-  
+
   def answer
     @answer = params[:answer]
+    @ask = coach_answer(@ask)
+  end
+
+  def coach_answer(message)
+    if message == 'Im going to work!'
+      'Great!'
+    elsif message.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
   end
 end
